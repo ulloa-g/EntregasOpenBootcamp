5aec52b5e22a2d0cf209b39a7c266e06c0a52ad9
ulloagabriel <ulloasaavedragabriel@gmail.com>
2022-08-01 09:22:53 -0300

    trabajando con tipos de datos

---
+++ javascript/02_creaLista/index.js
@@ -0,0 +1,3 @@
+let lista1 = ['gabriel', 28, true, cumple = new Date('august 24 1993'), favoriteBook = {tittle: 'El club de las 5am', author:'Robin Sharma', date:2015, url:'https://www.amazon.com/-/es/El-Club-5-manana-audiolibro/dp/B084TBZ4V8/ref=sr_1_1?crid=2OC9Y19QHXDG7&keywords=el+club+de+las+5+de+la+ma%C3%B1ana+libro&qid=1659356153&sprefix=el+c%2Caps%2C314&sr=8-1'}]
+
+console.log(lista1)
