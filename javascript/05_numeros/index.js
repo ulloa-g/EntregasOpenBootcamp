16b6c0322d3bfdee467048d7521ddca7f7e18cbd
ulloagabriel <ulloasaavedragabriel@gmail.com>
2022-08-08 21:03:42 -0300

    no entendi dos ejercicios, resolver luego.

---
+++ javascript/05_numeros/index.js
@@ -0,0 +1,15 @@
+/* Crea un archivo JS que contenga las siguientes líneas
+- Una variable que contenga tu altura en centímetros (entero)
+- Una variable que contenga tu altura en metros (número de coma flotante)
+- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+- Una variable que contenga tu altura en metros redondeada hacia arriba
+- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript */
+
+let alturaCm = 177;
+let alturaMetro = 1.77;
+let peso = 88.7;
+let alturaRedondeada = alturaMetro.toFixed(0);  // string redondeado hacia arriba
+
+/* No recuedo como redondear hacia abajo */
+/* No entendi la ultima consigna del ejercicio */
