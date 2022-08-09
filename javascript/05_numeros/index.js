d259898d47586a72d9c3287209bc07b15f50acde
ulloagabriel <ulloasaavedragabriel@gmail.com>
2022-08-08 21:06:30 -0300

    resueltas las dudas Repasar

--- javascript/05_numeros/index.js
+++ javascript/05_numeros/index.js
@@ -13,3 +13,14 @@ let alturaRedondeada = alturaMetro.toFixed(0);  // string redondeado hacia arrib
 
 /* No recuedo como redondear hacia abajo */
 /* No entendi la ultima consigna del ejercicio */
+
+/* Asi se resolvio en OpenBootcamp */
+
+const altura_cm = 171
+const altura_m = 1.71
+const peso_kg = 70.2
+
+const altura_red = Math.ceil(altura_m)
+const peso_red = Math.floor(peso_kg)
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
