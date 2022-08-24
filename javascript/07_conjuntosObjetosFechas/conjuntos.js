e4009f3629dd59e815ae664387840437e114e88c
ulloagabriel <ulloasaavedragabriel@gmail.com>
2022-08-24 15:33:04 -0300

    easy to get the assigment done

---
+++ javascript/07_conjuntosObjetosFechas/conjuntos.js
@@ -0,0 +1,5 @@
+// Un nuevo Set con los nombres de tu familia
+let familyNames = new Set(['eduardo', 'sandra', 'karina', 'claudio', 'gabriel','flor']);
+familyNames.add('gabriel');
+familyNames.add('Javascript');
+console.log(familyNames);
